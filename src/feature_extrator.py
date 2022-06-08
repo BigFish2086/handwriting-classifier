e144d0b46ace7ea6011958a6ae829b91152d74d8
BigFish2086 <a8686.ibrahim@gmail.com>
2022-06-08 04:59:02 +0200

    update feature_extrator.py
    Adjust it so that it could be used flexibly inside the project

--- src/feature_extrator.py
+++ src/feature_extrator.py
@@ -11,21 +11,34 @@ import numpy as np
 import os
 from tqdm import tqdm
 
-if __name__ == '__name__':
+
+def parse_args():
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument("--input_folder", type=str, default=r"<IMAGES FOLDER IMAGES ARE STORED>")
-    parser.add_argument("--output_folder", type=str, default=r"<FOLDER WHERE FEATURES ARE STORED")
+    parser.add_argument("-in", "--input_folder", type=str, default=r"<IMAGES FOLDER IMAGES ARE STORED>")
+    parser.add_argument("-out", "--output_folder", type=str, default=r"<FOLDER WHERE FEATURES ARE STORED")
     parser.add_argument("--sharpness_factor", type=int, default=10)
     parser.add_argument("--bordersize", type=int, default=3)
     parser.add_argument("--show_images", type=bool, default=False)
     parser.add_argument("--is_binary", type=bool, default=True)
     opt = parser.parse_args()
-    
-    input_folder = opt.input_folder
-    output_folder = opt.output_folder
-    
+    return opt
+
+
+def image_extract_hing_feat(img_path, show_images=False, is_binary=True, bordersize=3, sharpness_factor=10):
+    hinge = Hinge(show_images, is_binary, bordersize, sharpness_factor)
+    h_f = hinge.get_hinge_features(img_path)
+    return h_f
+
+
+def image_extract_cold_feat(img_path, show_images=False, is_binary=True, bordersize=3, sharpness_factor=10):
+    cold = Cold(show_images, is_binary, bordersize, sharpness_factor)
+    c_f = cold.get_cold_features(img_path)
+    return c_f
+
+
+def extract_feat(input_folder, sharpness_factor=10, bordersize=3, show_images=False, is_binary=True):
     class_dirs = os.listdir(input_folder)
-    
+
     class_dirs.sort()
     print(class_dirs)
 
@@ -34,19 +47,19 @@ if __name__ == '__name__':
     labels = []
     label_names = []
     ecount = 0
-    
-    cold = Cold(opt)
-    hinge = Hinge(opt)
-    
+
+    cold = Cold(show_images, is_binary, bordersize, sharpness_factor)
+    hinge = Hinge(show_images, is_binary, bordersize, sharpness_factor)
+
     for i, class_dir in enumerate(class_dirs):
         img_filenames = os.listdir(os.path.join(input_folder, class_dir))
         for img_filename in tqdm(img_filenames):
             try:
                 img_path = os.path.join(input_folder, class_dir, img_filename)
-                
+
                 h_f = hinge.get_hinge_features(img_path)
                 c_f = cold.get_cold_features(img_path)
-                
+
                 hinge_feature_vectors.append(h_f)
                 cold_feature_vectors.append(c_f)
                 label_names.append(class_dir)
@@ -54,17 +67,30 @@ if __name__ == '__name__':
             except Exception as inst:
                 ecount += 1
                 if ecount % 20 == 0:
-                    print(inst, f'error count: {ecount}')
+                    print(inst, f"error count: {ecount}")
                 continue
-        
+
         print(f"[STATUS] processed folder: {class_dir}")
-        
-    np.save(os.path.join(output_folder, f"hinge_features.npy"), hinge_feature_vectors)
-    np.save(os.path.join(output_folder, f"cold_features.npy"), cold_feature_vectors)
-    np.savez(os.path.join(output_folder, f"labels"), label = labels, label_name = label_names)
-    
-    print(f"Saved all hinge and cold features")
-           
+
+    return hinge_feature_vectors, cold_feature_vectors, labels, label_names
 
 
+def save_feat(output_folder, hinge_feature_vectors, cold_feature_vectors, labels, label_names):
+    np.save(os.path.join(output_folder, "hinge_features.npy"), hinge_feature_vectors)
+    np.save(os.path.join(output_folder, "cold_features.npy"), cold_feature_vectors)
+    np.savez(os.path.join(output_folder, "labels"), label=labels, label_name=label_names)
+    print("Saved all hinge and cold features")
 
+
+if __name__ == "__main__":
+
+    # Parse arguments
+    # opt = parse_args()
+
+    # hinge_feature_vectors, cold_feature_vectors, labels, label_names = extract_feat(
+    #     opt.input_folder, opt.sharpness_factor, opt.bordersize, opt.show_images, opt.is_binary
+    # )
+    # save_feat(opt.output_folder, hinge_feature_vectors, cold_feature_vectors, labels, label_names)
+
+    hinge_feature_vectors, cold_feature_vectors, labels, label_names = extract_feat("./preprocessed/data-set/")
+    save_feat("./out", hinge_feature_vectors, cold_feature_vectors, labels, label_names)
